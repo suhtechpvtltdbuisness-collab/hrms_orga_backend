f0015d1dc05e5d53644c82a406e3bfd64a99b03f
GautamKumarhr7 <kumargautam3231@gmail.com>
2026-01-13 16:47:40 +0530

    basic setup

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+import router from "./router.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+export default app;
